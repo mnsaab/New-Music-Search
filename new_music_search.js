9da678a461679daf9b349ea376233f9e265e5329
Mazzen Saab <mnsaab@umich.edu>
2020-04-08 16:49:06 -0500

    starter files for spotify search

---
+++ new_music_search.js
@@ -0,0 +1,6 @@
+var app = angular.module('newMusicSearch', []);
+// https://accounts.spotify.com/en/authorize?client_id=89ce701ef13d43389301f57126506b0f&response_type=token&redirect_uri=file:%2F%2F%2FC:%2FUsers%2Fmazze%2FDevelopment%2Fnew_music_search%2Findex.html
+
+app.controller('newReleases', ['$scope', '$http', function ($scope, $http) {
+	continue
+}]);
