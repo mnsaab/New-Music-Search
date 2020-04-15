52b3d827b3867cd6e002ece38ce21548f0d01c7f
Mazzen Saab <mnsaab@umich.edu>
2020-04-14 22:27:07 -0500

    correctly communicating with spotify and retrieving releases

--- new_music_search.js
+++ new_music_search.js
@@ -1,5 +1,4 @@
 var app = angular.module('newMusicSearch', []);
-// https://accounts.spotify.com/en/authorize?client_id=89ce701ef13d43389301f57126506b0f&response_type=token&redirect_uri=https:%2F%2Fmnsaab.github.io%2FNew-Music-Search%2F
 
 app.controller('newReleases', ['$scope', '$http', '$location', '$window', 
 function ($scope, $http, $location, $window) {
@@ -16,5 +15,29 @@ function ($scope, $http, $location, $window) {
 			$window.location.href = 'https://accounts.spotify.com/en/authorize?client_id=89ce701ef13d43389301f57126506b0f&response_type=token&redirect_uri=https:%2F%2Fmnsaab.github.io%2FNew-Music-Search%2F';
 		}
 	};
+
+	$scope.number = 50;
+
+	$scope.getReleases = function() {
+// $scope.token = "BQDzuGHQpdogZYKoaCW1kkxtpMZx4E96Lb3RolSXiZ68rqW4Wx3D-hUJu4weeTPaQwz1GENuYWUqrywz2ahKwb4q45UqNQDkJ-J1pZNuRdEWlILVLgDY1ska08KUO-3CR7XreiOU--V8Ur_VMjC9fs_YzuFvNDQ";
+		let limit = 20;
+		if ($scope.numberOfResults){
+			limit = $scope.numberOfResults;	
+		}
+		$http.get('https://api.spotify.com/v1/browse/new-releases?limit=' + limit, {
+			headers: {
+				"Authorization": 'Bearer ' + $scope.token
+			}
+		}).success(function(response){
+			$scope.results = response.albums;
+			console.log($scope.results);
+		})
+		.error(function (response) {
+			if (response["error"]["message"] == "The access token expired"){
+				$window.location.href = 'https://accounts.spotify.com/en/authorize?client_id=89ce701ef13d43389301f57126506b0f&response_type=token&redirect_uri=https:%2F%2Fmnsaab.github.io%2FNew-Music-Search%2F';
+			}
+			alert(response["error"]["message"]);
+		});;
+	}
 	
 }]);
